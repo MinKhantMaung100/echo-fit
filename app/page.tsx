262bc5c6096e4ab25c13eb050be1aa7020d30c6a
MinKhantMaung100 <59551622+MinKhantMaung100@users.noreply.github.com>
2025-03-15 13:53:08 +0000

    second

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,180 @@
+"use client";
+
 import Image from "next/image";
+import { Suspense, useEffect, useRef, useState } from "react";
+import Liner from "@/public/svg/liner.svg";
+import DisplayPoster from "@/public/webp/one.png";
+import HeroImage from "@/public/webp/hero-image.webp";
+import { AnimatePresence, motion } from "framer-motion";
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "model-viewer": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const containerRef = useRef(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const modelViewerRef = useRef(null);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    import("@google/model-viewer");
+
+    const modelViewer = modelViewerRef.current;
+    console.log(modelViewer);
+    if (!modelViewer) return;
+
+    modelViewer.addEventListener("progress", (event) => {
+      const loadProgress = Math.floor(event.detail.totalProgress * 100);
+      setProgress(loadProgress);
+    });
+  }, []);
+
+  return (
+    <section className="flex flex-col h-screen ">
+      {/* 3d Model here */}
+      <div className="relative w-full min-h-[60%]" ref={containerRef}>
+        {/* <Canvas>
+          <ambientLight intensity={Math.PI / 2} />
+          <directionalLight position={[10, 10, 5]} intensity={1} />
+          <Suspense fallback={<Loader />}>
+            <Model url="/glb/shoes-display-shelf.glb" />
+            <OrbitControls />
+          </Suspense>
+        </Canvas> */}
+
+        <ModelViewer
+          src="glb/shoes-display-shelf.glb"
+          ref={modelViewerRef}
+          alt="3D Model"
+          background-color="red"
+          tone-mapping="aces"
+          with-spotlight
+          camera-controls
+          camera-orbit="0deg 87deg 1m"
+          camera-target="0m 5.1m 1m"
+          field-of-view="14deg"
+          exposure="1"
+          // Limit horizontal rotation (left-right)
+          min-camera-orbit="0deg 0deg auto"
+          max-camera-orbit="10deg 100deg 20m"
+          // Limit zoom distance
+
+          style={{
+            width: "100%",
+            height: "100%",
+            background:
+              "linear-gradient(18deg, rgba(230,241,244,1) 0%, rgba(255,255,255,1) 100%)",
+          }}
+          interpolation-decay="200"
+          rotation-per-second="0"
+        >
+          <AnimatePresence>
+            {progress >= 100 ? null : <LoadingShelf progress={progress} />}
+          </AnimatePresence>
+          <button
+            slot="hotspot-1"
+            data-position="2.9 5 1"
+            data-normal="0 1 0"
+            className="relative w-fit h-fit"
           >
+            <span className="relative flex size-3">
+              <span className="absolute inline-flex h-full w-full animate-ping-2s rounded-full bg-blue-100 opacity-75 duration-1000"></span>
+              <span className="relative inline-flex size-3 rounded-full bg-blue-100"></span>
+            </span>
+          </button>
+        </ModelViewer>
+      </div>
+
+      {/* Shoe Preview */}
+      <section></section>
+
+      {/* Content */}
+      <div className="bg-ambe-100 w-full min-h-[40%] py-8 px-16 flex flex-col justify-between">
+        {/* Main Area */}
+        <div className="flex items-center">
+          <div className="w-[60%]">
+            <h1 className="text-4xl font-urbanist font-urbanist-italic-semibold leading-12 text-my-primary b-red-300 w-fit flex flex-col gap-0">
+              <span>Try-View-Buy</span>
+              <span>Experience Shoes in 3D</span>
+            </h1>
+            <p className="text-xl pt-2 text-my-primary">
+              Step into the Future with EcoFit's Virtual
+              <br />
+              Shoe Experience.
+            </p>
+          </div>
+
+          <div className="w-[40%] bg-aber-200 overflow-hidden flex justify-end items-center b-amber-200">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={HeroImage}
+              alt="hero-image.webp"
+              className="w-[16rem]"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {/* Footer */}
+        <div>
+          <Image src={Liner} alt="footer-liner-svg" className="w-[14rem]" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function Model({ url }) {
+  const { scene } = useGLTF(url);
+  return <primitive object={scene} />;
+}
+
+function Loader() {
+  const { progress } = useProgress();
+  return <Html center>{progress} % loaded</Html>;
+}
+
+function ModelViewer(props: any) {
+  useEffect(() => {
+    // Dynamically import the web component on the client side
+    import("@google/model-viewer");
+  }, []);
+
+  return <model-viewer {...props} />;
+}
+
+function LoadingShelf({ progress }) {
+  return (
+    <div
+      slot="progress-bar"
+      className="absolute w-full h-full bg-white bg-opacity-70 rounded-lg text-center shadow-md"
+    >
+      <div className="h-full bg-gray-200 rounded overflow-hidden my-1">
+        <motion.div className="w-full h-full relative">
+          <motion.div animate={{ opacity: `${progress}%` }}>
+            <Image
+              src={DisplayPoster}
+              alt="footer-liner"
+              className="w-full h-full relative z-10"
+            />
+          </motion.div>
+
+          <motion.div
+            className={`w-full h-full absolute top-0 left-0 bg-gradient-to-r from-blue-400 to-blue-500 z-20 transition-all duration-500`}
+            animate={{ width: `${100 - progress}%` }}
+            transition={{
+              type: "spring",
+              stiffness: 100,
+            }}
+          ></motion.div>
+        </motion.div>
+      </div>
+      <p className="my-1 text-sm text-gray-800">Loading: {progress}%</p>
     </div>
   );
 }
